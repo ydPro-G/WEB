2c19546c1568a809c08f21a383eff397b0d4bfb8
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-09-17 16:05:49 +0800

    update

--- HTML/guanwang/js/index.js
+++ HTML/guanwang/js/index.js
@@ -1,9 +1,44 @@
 var jq = $.noConflict();
 
 jq(function(){
-    jq("div.img").hover(function(){
-        jq("div.img").css("-webkit-box-shadow", "-1px 0px 20px 2px rgba(0,0,0,0.75)",
-        "-moz-box-shadow","-1px 0px 20px 2px rgba(0,0,0,0.75)",
-        "box-shadow","-1px 0px 20px 2px rgba(0,0,0,0.75)",)
+    jq("div#1").mouseenter(function(){
+        jq("div#1").css("box-shadow","10px 10px 6px #ccc")
+    });
+    jq("div#1").mouseleave(function(){
+        jq("div#1").css("box-shadow","0px 0px 6px #ccc")
+    });
+
+    jq("div#2").mouseenter(function(){
+        jq("div#2").css("box-shadow","10px 10px 6px #ccc")
+    });
+    jq("div#2").mouseleave(function(){
+        jq("div#2").css("box-shadow","0px 0px 6px #ccc")
+    });
+
+    jq("div#3").mouseenter(function(){
+        jq("div#3").css("box-shadow","10px 10px 6px #ccc")
+    });
+    jq("div#3").mouseleave(function(){
+        jq("div#3").css("box-shadow","0px 0px 6px #ccc")
+    });
+
+    jq("div#4").mouseenter(function(){
+        jq("div#4").css("box-shadow","10px 10px 6px #ccc")
+    });
+    jq("div#4").mouseleave(function(){
+        jq("div#4").css("box-shadow","0px 0px 6px #ccc")
+    });
+
+    jq("div#5").mouseenter(function(){
+        jq("div#5").css("box-shadow","10px 10px 6px #ccc")
+    });
+    jq("div#5").mouseleave(function(){
+        jq("div#5").css("box-shadow","0px 0px 6px #ccc")
+    });
+});
+
+jq(function(){
+    jq("a").mouseenter(function(){
+        jq(a).css("text-decoration","underline")
     })
 })
