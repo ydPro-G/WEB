1fb1d723a99cae37ecbd37e67bc2c1e66844d37f
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2021-02-03 16:10:59 +0800

    Update basis.js

--- js/basis/basis.js
+++ js/basis/basis.js
@@ -44,6 +44,11 @@ let isBoss = confirm("Are you the boss?"); //如果按下 OK 则结果返回 tru
 
 
 
+
+
+
+
+
 // if, else if, else, ?
 let year = prompt('year?','');
 
@@ -62,10 +67,20 @@ if (year < 2015) {
 
 
 
+
+
+
+
+
 //  逻辑运算符
 result = a || b; //逻辑或,有一个为true就为true,否则就是flase
 result = a && b; //逻辑与,两个都为真时返回true,否则返回flase
 result = !value; //逻辑非,不等于
+
+
+
+
+
 
 
 
@@ -112,6 +127,12 @@ for (let i = 0; i < 10; i++) {
 
 
 
+
+
+
+
+
+
 // switch语句
 // switch ,case,break
 let b = 2 + 2;
@@ -133,6 +154,13 @@ switch(b) {
 
 
 
+
+
+
+
+
+
+// 函数
 let userName = 'Jonh';
 // 定义函数
 function showMessage() {
@@ -144,6 +172,7 @@ function showMessage() {
 showMessage(); //未调用之前showMessage是jonh,调用后userName是Bob
 
 
+//  function return
 function checkAge(age) {
     if (age > 18) {
         return true;
@@ -157,5 +186,67 @@ let age = prompt('多大了?',18);
 if (checkAge(age)) {
     alert('可以访问');
 } else {
-    alert('不可以访问')
+    alert('不可以访问');
 }
+
+// 命名函数：用一种语言前缀来开始一个函数，它模糊地描述了动作。
+show... 显示什么
+get...  返回什么
+calc... 计算什么
+create... 创建什么
+check... 检查什么
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 在js中函数只是一个值，它可以赋值给变量
+let sayHi = function() {
+    alert('hello');
+};
+alert(sayHi); //显式函数代码
+
+function sayBey() {
+    alert('hello');
+};
+// 函数可以复制。不带括号复制时复制的是函数本身，带括号复制时复制的是函数的调用结果。
+let func = sayBey;
+
+// 函数声明作为代码块，最后的}不带分号，函数表达式作为值使用，所以最后的}带；号
+
+function ask(question, yes, no) {
+    if(confirm(question)) yes()
+    else no();
+}
+
+function showOk() {
+    alert('同意');
+}
+
+function showNO() {
+    alert('拒绝');
+}
+// 函数被调用，js中称为回调函数
+ask('Dou you agree?',showOk,showNO);
+
+/* 
+函数表达式和函数声明的区别：
+函数表达式是在执行流到达它的时候才创建，并且从那时才是可用的。
+函数声明在整个脚本/代码块都是可用的。
+*/
+
+/* 
+函数表达式产生的原因：
+解决当一个函数声明在一个代码块中进行时，它在代码块中可见，在代码块外不可见的问题。
+使用函数表达式，将函数赋值给在 if 语句之外声明的变量，这样就可以在代码块外使用。
+ */
